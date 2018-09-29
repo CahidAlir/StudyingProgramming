ca17e48faa88a8bf5fc22e4ea0d4b67a7e5df174
Al-Said <aalir.said@gmail.com>
2018-09-29 19:02:33 +0300

    sorduğun sorunun cevabı

--- ExampleNo1.c
+++ ExampleNo1.c
@@ -1,8 +1,60 @@
 #include <stdio.h>
+#define ADD 1
+#define SUB 2
+#define MULT 3
+#define DIV 4
+
+int showMenu();
+
 int main () {
 
-    printf("Hello World!");
-    //Bu yaptığım ilk değişiklik..
-    //Bunu değiştirip sana gönderceğim...	
+	float result = 0;
+	float num1, num2;
+
+	int menu = showMenu();
+	while(menu<1 || menu >4) {
+		menu = showMenu();
+	}
+
+	printf("Enter the first variable\n");
+	scanf("%f",&num1);
+	printf("Enter the second variable\n");
+	scanf("%f",&num2);
+
+	switch(menu) {
+		
+		case ADD:
+		result = num1 + num2;
+		break;
+
+		case SUB:
+		result = num1 - num2;
+		break;
+
+		case MULT:
+		result = num1 * num2;
+		break;
+
+		case DIV:
+		result = num1 / num2;
+		break;
+	}
+
+	printf("\nResult is\n----------------------------\n");
+	printf("%.2f",result);
+	printf("\n----------------------------\n\n");
     return 0;
+}
+
+int showMenu() {
+
+	int menu = 0;
+	printf("Select Operation : \n");
+	printf("1. Addition\n");
+	printf("2. Substraction\n");
+	printf("3. Multiplication\n");
+	printf("4. Division\n");
+
+	scanf("%d" ,&menu);
+	return menu;
 }
