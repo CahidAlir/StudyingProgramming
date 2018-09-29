8ed3a90842057d2f67da09d3398ab004002d6135
Cahid Alır <alircahid@gmail.com>
2018-09-29 19:55:10 +0300

    BMI calculation app

--- ExampleNo2.c
+++ ExampleNo2.c
@@ -3,9 +3,49 @@ AUTHOR = CAHID ALIR
 IF/ELSE EGZERSIZI
 */
 #include <stdio.h>
+#define KG 1
+#define POUND 2
+int showMenu();
 
 int main()
 {
+    float result = 0;
+    float num1, num2;
+    int menu;
+    menu = showMenu();
+
+    while(menu<1 || menu>2);
+{
+    menu = showMenu();
+}
+    printf("Enter your weight: \n");
+    scanf("%f", &num1);
+    printf("enter your height: \n");
+    scanf("%f", &num2);
+
+    switch(menu)
+{
+        case KG:
+        result = num1 / num2 * num2;
+        break;
+
+        case POUND:
+        result = num1 * 703 / num2 * num2;
+        break;
+}
+
+    printf("your BMI is %.2f",result);
 
     return 0;
+}
+int showMenu()
+{
+
+	int menu = 0;
+	printf("Select Units : \n");
+	printf("1.Kilograms \n");
+	printf("2.Pounds \n");
+//?&menu
+	scanf("%d" ,&menu);
+	return menu;
 }
