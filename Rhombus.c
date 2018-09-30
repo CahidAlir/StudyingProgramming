4a91a80ac93f68468d1b76f9b793029b73ad9ecf
Said <aalir.said@gmail.com>
2018-09-30 12:33:26 +0300

    Your homework is ready

---
+++ Rhombus.c
@@ -0,0 +1,26 @@
+/**
+Bu example'da yapmanı istediğim şükül şu
+
+
+    *
+   ***
+  *****
+ *******
+*********
+
+Sadece 1 adet yıldız, bir adet boşluk ve bir adet '\n' kullan
+yaptığın çözümü plz printShape() fonksiyonunun içine yaz.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	printShape();
+	return 0;
+}
+
+void printShape()
+{
+	//Çözümün buraya!!!
+}
