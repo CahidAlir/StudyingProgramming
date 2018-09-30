f47e0317e9f52963920026bb5b76cbbf183d711e
Cahid Alır <alircahid@gmail.com>
2018-09-30 03:27:28 +0300

    ekstra açıklama

--- Loop Example.c
+++ Loop Example.c
@@ -12,7 +12,7 @@ void doWhileloop();
 int main(void)
 {
 	//3 çeşit döngü (loop) türü var...
-	//Bunların 3'ü de hemen hemen aynı işi yapar ancak duruma göre farklılık gösterir.. 
+	//Bunların 3'ü de hemen hemen aynı işi yapar ancak duruma göre farklılık gösterir..
 	//3 farklı fonksiyonda bu 3 loop'u incelemeni istiyorum..
 
 	forLoop();
@@ -30,7 +30,7 @@ For döngüsünü anlatan fonksiyon.. Burada benim yaptığım gibi her zaman
 Koduna düzgün açıklamalar girmeyi ihmal etme!...
 */
 void forLoop()
-{ 
+{
 	//for loop şöyle çalışıyor
 	int counter; //adı üzerinde sayaç
 
@@ -48,16 +48,26 @@ void forLoop()
 	//DİKKAT.. Koşul doğru olduğu sürece döngü çalışır.. Eğer koşul hep doğru olursa
 	//buna runtime error olan infinite loop denir ve alınacak en aptal sonuçlardan biridir...
 	//NOT: 1 arttırırken counter++, bir azaltırken counter-- yazabilirsin..
+
+  /*
+    Aynısının laciverti
+	counter = 0;
+	while (counter < 10)
+    {
+        printf("%d", counter+1);
+        counter++;
+    }
+*/
 }
 
-void whileLoop() 
+void whileLoop()
 {
 	int option = 0;
 	//Bu döngü çeşiti daha çok döngünün kaç defa değil de hangi koşulda çıkması gerektiğini
 	//biliyorsan kullanman gereken bir döngü çeşidi..
 	//while'ın içinde yazılı olan işlem n defa döner.. N sayısını kullanıcı ya da başka bir işlem
 	//belirleyebilir..
-	while(option != -1) 
+	while(option != -1)
 	{
 		printf("Çıkmak için -1'e bas.. Onun dışında hep bu yazı gelecek!!\n" );
 		scanf("%d",&option);
@@ -68,7 +78,7 @@ void doWhileloop()
 {
 	getchar();
 	int option = -1;
-	do 
+	do
 	{
 		//Do while loop while loop'a çok ama çok benzer.. tek farkı
 		//Do while loop'da işlem EN AZ 1 defa koşula BAKMAKSIZIN YAPILIR!...
