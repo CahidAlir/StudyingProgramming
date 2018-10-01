eb22cd121d0c41fce4486c730c0534a59c812675
Al-Said <aalir.said@gmail.com>
2018-10-01 13:05:41 +0300

    ipucu

--- Rhombus.c
+++ Rhombus.c
@@ -32,6 +32,10 @@ void printShape()
         for(star=4; 0<star<nLine; star--)
         {
             printf("*",star * 2 + 1);
+	    //Böyle bir şey yok.. ama sana tip vereyim.. star sayısını azaltmak ya da arttırmak yerine
+	    //Sonuçta lineer artan bir durum var.. sen de lineer arttır...
+	    //Yani gidip star-- demek yerine (Yani star sayısını bir arttırmak yerine
+	    //1,3,5,7,9 diye artacak şekilde implemente et...
         }
          printf("\n");
     }
