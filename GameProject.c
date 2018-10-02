4feb5ceaa87014e51a01921fe46e3a27cc315398
Al-Said <aalir.said@gmail.com>
2018-10-02 22:11:39 +0300

    Here we go!

---
+++ GameProject.c
@@ -0,0 +1,51 @@
+/**
+That's your new project..
+
+Create a game..  Program should generate a number between 1-10.000
+Than gives player ten rights to guess.
+Your program should say "your number is bigger/smaller than my number"
+Program should rate all guess.. The closer guess player did, the much more point that he gets..
+I totally leave it to you.. Do whatever you like but just try each guess should be promoted between 1-50
+
+when user find out the number that program generates, than decide:
+
+If player guess the correct number less than 3 times than
+
++10.000 point and print "Sorry god, I couldn't recognize you!"
+
+between 3-5
+
++7.500 point and print "Oracle!! Is that you?"
+
+5-7 guesses
+
++5.000 point and print "Brilliant! You are good at it!"
+
+7-9 guess
+
++1.000 point and print "Well done boy!"
+
+10 guess 
+
++200 point and print "You lucky bastard!"
+
+and each case should finish with this statement
+
+"YOUR TOTAL SCORE IS : XXXX"
+
+You can google it if you wonder how a c program generates a number..
+
+Good Luck!
+*/
+
+
+
+#include <stdio.h>
+
+int main()
+{
+	//Try to code understandable and maintainable..
+	//If I were you, I'd create a function for every single different work!
+	
+	return 0;
+}
