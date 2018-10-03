a6ef62560654083a2bd03e61b38590b5457cd715
Cahid Alır <alircahid@gmail.com>
2018-10-03 19:07:53 +0300

    take a look

--- GameProject.c
+++ GameProject.c
@@ -25,7 +25,7 @@ between 3-5
 
 +1.000 point and print "Well done boy!"
 
-10 guess 
+10 guess
 
 +200 point and print "You lucky bastard!"
 
@@ -41,11 +41,94 @@ Good Luck!
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generateNum();
+int cases();
 
 int main()
 {
 	//Try to code understandable and maintainable..
 	//If I were you, I'd create a function for every single different work!
-	
-	return 0;
+
+	int counter, score;
+	int gss1, gss2, gss3, gss4, gss5, gss6, gss7, gss8, gss9, gss10;
+	float scr1, scr2, scr3, scr4, scr5, scr6, scr7, scr8, scr9, scr10;
+
+    srand(time(NULL));
+    int r = rand()%10000+1;
+    printf("%d", r);
+    //this is for to learn if it works
+    printf("Make a guess\n");
+    scanf("%d",&gss1);
+
+    if(gss1==r)
+        {
+        printf("Sorry god, I couldn't recognize you!\n");
+        printf("YOUR TOTAL SCORE IS : 10000");
+        return 0;
+        }
+    if(gss1<r)
+    {
+        printf("your guess is smaller then my number\n");
+        printf("make another guess\n");
+        scanf("%d", gss2);
+        scr1= 100 / (r - gss1);
+    }
+    if(gss1>r)
+    {
+        printf("your guess is bigger then my number\n");
+        printf("make another guess\n");
+        scanf("%d", gss2);
+        scr1= 100 / (gss1 - r);
+    }
+    if(gss2==r)
+    {
+        printf("Sorry god, I couldn't recognize you!\n");
+        printf("YOUR TOTAL SCORE IS : 10000");
+        return 0;
+    }
+    if(gss2<r)
+    {
+        printf("your guess is smaller then my number\n");
+        printf("make another guess\n");
+        scanf("%d", gss3);
+        scr2= 100 / (r - gss2);
+    }
+    if(gss2>r)
+    {
+        printf("your guess is bigger then my number\n");
+        printf("make another guess\n");
+        scanf("%d", gss3);
+        scr2= 100 / (gss2 - r);
+    }
+    if(gss3==r)
+    {
+        printf("Oracle!! Is that you?\n");
+        printf("YOUR TOTAL SCORE IS : %f", score);
+        return 0;
+    }
+    if(gss3<r)
+    {
+        printf("your guess is smaller then my number\n");
+        printf("make another guess\n");
+        scanf("%d", gss4);
+        scr3= 100 / (r - gss3);
+    }
+    if(gss3>r)
+    {
+        printf("your guess is bigger then my number\n");
+        printf("make another guess\n");
+        scanf("%d", gss4);
+        scr3= 100 / (gss3 - r);
+    }
+    if(gss4==r)
+    {
+
+    }
+    return 0;
 }
+
+
+
