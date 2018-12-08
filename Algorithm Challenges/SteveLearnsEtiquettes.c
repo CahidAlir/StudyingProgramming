3a95296636acb3c403bf620b6f8f0551d24807d1
Cahid Alır <alircahid@gmail.com>
2018-12-08 22:14:00 +0200

    Done

--- Algorithm Challenges/SteveLearnsEtiquettes.c
+++ Algorithm Challenges/SteveLearnsEtiquettes.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    char sentence[100];
+    int i=0, n, numberOfChars=0;
+
+    printf("Enter the sentence:");
+    gets(sentence);
+
+    while(sentence[i]!='\0')
+    {
+        numberOfChars++;
+        i++;
+    }
+
+    if(sentence[numberOfChars-1]=='?')
+    {
+        for(n=1;n<=numberOfChars;n++)
+        {
+            if(n==numberOfChars)
+            {
+                printf(", please?");
+                return 0;
+            }
+            printf("%c",sentence[n-1]);
+        }
+    } else {
+        printf("No job for me I guess.\n\n");
+        main();
+    }
+}
 
