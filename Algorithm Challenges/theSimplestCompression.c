bf8d97a4102082516ca817473a16f3577c82ba48
Cahid Alır <alircahid@gmail.com>
2018-12-13 23:09:50 +0200

    change of path added(non-complete)

--- Algorithm Challenges/theSimplestCompression.c
+++ Algorithm Challenges/theSimplestCompression.c
@@ -1,34 +1,58 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void convertString(char[]);
+void convertString(char[], int);
+int countElements(char[]);
 
 int main()
 {
     char givenString[100];
+    int elementsOfArray;
 
     gets(givenString);
 
-    convertString(givenString);
+    elementsOfArray=countElements(givenString);
+
+    convertString(givenString, elementsOfArray);
 
     return 0;
 }
 
-void convertString(char givenString[])
+int countElements(char givenString[])
+{
+    int i=0, counter=0;
+
+    while(givenString[i]!='\0')
+    {
+        counter++;
+        i++;
+    }
+    return counter;
+}
+
+void convertString(char givenString[], int elementsOfArray)
 {
     char alteredNumber[1];
-    int i=0, repeated, counter=0;
+    int i=0, counter=1;
+    int position;
+    char repeated;
 
     while(givenString[i]!='\0')
     {
         if(givenString[i]==givenString[i+1])
         {
-            for(repeated=givenString[i];repeated!=givenString[i+1];i++)
+            int j=i;
+
+            for(repeated=givenString[i];repeated==givenString[j+1];i++)
             {
                 counter++;
                 sprintf(alteredNumber, "%d",counter);
                 givenString[i+1]=alteredNumber[0];
             }
-            i+=counter;
+            for(position=i+1;position<elementsOfArray;position++)
+            {
+                givenString[i+2]=givenString[i+3];
+            }
         }
         i++;
     }
