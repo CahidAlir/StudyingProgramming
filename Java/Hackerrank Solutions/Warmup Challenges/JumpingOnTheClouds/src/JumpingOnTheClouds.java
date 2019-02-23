e58d0a4c07609da3b8af36a8aa44d5643a6f3beb
Cahid Alır <alircahid@gmail.com>
2019-02-23 15:54:59 +0200

    Folder re-orde Hackerrank solution add Name convention

---
+++ Java/Hackerrank Solutions/Warmup Challenges/JumpingOnTheClouds/src/JumpingOnTheClouds.java
@@ -0,0 +1,25 @@
+
+public class JumpingOnTheClouds {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 0, 0, 0, 0, 1, 0 };
+		System.out.println(jumpingOnClouds(arr));
+	}
+
+	private static int jumpingOnClouds(int[] c) {
+		int totalStep = 0;
+		int current = 0;
+
+		while (current < c.length - 1) {
+			if (current + 2 < c.length && c[current + 2] != 1) {
+				current += 2;
+			} else {
+				current++;
+			}
+			totalStep++;
+		}
+		return totalStep;
+	}
+
+}
---
+++ Java/Hackerrank Solutions/Warmup Challenges/SockMatching/src/SockMatching.java
@@ -0,0 +1,22 @@
+
+public class SockMatching {
+	 
+	static int sockMerchant(int n, int[] ar) {
+	        int matchs=0;
+
+	        for(int i=0;i<n;i++) {
+	        	if(ar[i]==0) {
+	        		continue;
+	        	}
+	            for(int j=i+1;j<n;j++) {
+	                if(ar[i]==ar[j]) {
+	                	matchs++;
+	                	ar[i]=0;
+	                	ar[j]=0;
+	                	break;
+	                }
+	            }
+	        }
+	        return matchs;
+	    }
+}
